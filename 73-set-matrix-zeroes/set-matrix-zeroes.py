c2424d7706422fed14e8834c58b9c681b39eeed8
Mohit Negi <mohitnegiog@gmail.com>
2024-07-03 14:33:11 +0530

    Time: 100 ms (71.71%) | Memory: 17.4 MB (51.21%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -5,15 +5,23 @@ class Solution:
         """
         m=len(matrix)
         n=len(matrix[0])
-        rows= [0]*m
-        cols=[0]*n
+        col0=1
         for i in range(m):
             for j in range(n):
                 if (matrix[i][j] == 0):
-                    rows[i]=1
-                    cols[j]=1
+                    matrix[i][0]=0
+                    if(j!=0):
+                        matrix[0][j]=0
+                    else:
+                        col0=0
         
-        for i in range(m):
-            for j in range(n):
-                if rows[i]==1 or cols[j]==1:
+        for i in range(1,m):
+            for j in range(1,n):
+                if matrix[i][0]==0 or matrix[0][j]==0:
                     matrix[i][j]=0
+        if(matrix[0][0]==0):
+            for i in range(n):
+                matrix[0][i]=0
+        if(col0==0):
+            for i in range(m):
+                matrix[i][0]=0
