1ae618e241b735bd49fafdb3bdf6f9392462f326
Mohit Negi <mohitnegiog@gmail.com>
2024-08-22 23:26:53 +0530

    Time: 3 ms (41.07%) | Memory: 7.3 MB (65.77%) - LeetSync

--- 476-number-complement/number-complement.cpp
+++ 476-number-complement/number-complement.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int findComplement(int num) {
-        int i = 0, ans = 0;
-        while (num) {
-            int temp = num & 1;
-            if (!temp)
-                ans += pow(2, i);
+        int x=0,i=0;
+        while(num){
+            int temp=num & 1;
+            if( !temp )
+                x += pow(2, i);
             num = num >> 1;
             i++;
-        }
-        return ans;
+        }return x;
     }
 };
